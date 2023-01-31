7ac86aafbaa20e71471141917280291662c7b5cc
sumi <sumipavib1302@gmail.com>
2023-01-31 22:04:58 +0530

    Added: UC1_GamblingSimulationProblemStart

--- src/com/gamlingsimulation/GamblingSimulationProblem.java
+++ src/com/gamlingsimulation/GamblingSimulationProblem.java
@@ -2,4 +2,10 @@ package com.gamlingsimulation;
 
 public class GamblingSimulationProblem {
 
+	public static void main(String[] args) {
+        System.out.println("Welcome in Gambling Simulation Problem");
+        int stake = 100;
+        int bet = 1;
+        System.out.println("start with a stake of " + stake +" every day and bet " + bet +" every game.");
+    }
 }
