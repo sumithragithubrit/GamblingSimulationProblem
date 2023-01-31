596f10ed89bfbb181a34529bb354951bb2a41be5
sumi <sumipavib1302@gmail.com>
2023-01-31 22:33:20 +0530

    Added:Each Month Know The Days won and lost by how much

--- src/com/gamlingsimulation/GamblingSimulationProblem.java
+++ src/com/gamlingsimulation/GamblingSimulationProblem.java
@@ -3,32 +3,48 @@ import java.util.Random;
 
 	public class GamblingSimulationProblem {
 		
-		public static void main(String[] args) {
-			 System.out.println("Welcome in Gambling Simulation Problem");
-		        int stake = 100;
-		        int bet = 1;
-		        int count = 0;
-		        Random random = new Random();
-		        System.out.println("Initial stake " + stake);
+		static int monthCount = 0;
+	    public static void main(String[] args) {
 
-		        int day = 20;
-		        for ( int i = 1;i <= day; i++ ){
-		            int num = random.nextInt(2);
 
-		            if (num == 1) {
-		                stake += bet;
-		                count++;
-		                System.out.println("gambler is win by day => "+count +  " & total Stake is " + stake);
+	        // GamblingSimulationProblem obj = new GamblingSimulationProblem();
+	        System.out.println("Welcome in Gambling Simulation Problem");
 
-		            } else if (num == 0) {
-		                stake -= bet;
-		                count++;
-		               System.out.println("gambler is loose by day => "+count +  " & total Stake is " + stake);
+	        int month = 2;
+	        for (int k = 1; k <= month; k++) {
+	             monthCount++;
+	            everyDayWonOrLost();
 
-		            }else {
-		                System.out.println("player stop gambling for day "  );
-		            }
+	        }
 
-		        }
 	    }
+
+	        public static void everyDayWonOrLost() {
+	             int stake = 100;
+	             int bet = 1;
+	             int count = 0;
+
+	            Random random = new Random();
+	            System.out.println("Initial stake month: "+monthCount+" => " + stake);
+
+	            int day = 20;
+	            for (int i = 1; i <= day; i++) {
+	                int num = (int) Math.floor(Math.random()*10) %2;
+
+	                if (num == 1) {
+	                    stake += bet;
+	                    count++;
+	                    System.out.println("gambler is win by day => " + count + " & total Stake is " + stake);
+
+	                } else  {
+	                    stake -= bet;
+	                    count++;
+	                    System.out.println("gambler is loose by day => " + count + " & total Stake is " + stake);
+
+	                }
+
+
+	            }
+	        }
+
 }
