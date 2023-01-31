df8499bbdeb316ff30332ad75e4d3b896c9b5293
sumi <sumipavib1302@gmail.com>
2023-01-31 22:10:52 +0530

    Added:As a Gambler make  bet so either win or loose

--- src/com/gamlingsimulation/GamblingSimulationProblem.java
+++ src/com/gamlingsimulation/GamblingSimulationProblem.java
@@ -1,11 +1,23 @@
 package com.gamlingsimulation;
+import java.util.Random;
 
-public class GamblingSimulationProblem {
+	public class GamblingSimulationProblem {
+		
+	    public static void main(String[] args) {
+	        System.out.println("Welcome in Gambling Simulation Problem");
+	        int stake = 100;
+	        int bet = 1;
+	        Random random = new Random();
+	       int num = random.nextInt(2) + 0;
 
-	public static void main(String[] args) {
-        System.out.println("Welcome in Gambling Simulation Problem");
-        int stake = 100;
-        int bet = 1;
-        System.out.println("start with a stake of " + stake +" every day and bet " + bet +" every game.");
-    }
+	        if (num == 1){
+	            stake += bet;
+	            System.out.println("gambler is win by "+ bet+" & total Stake is " + stake);
+
+	        }
+	        else if (num == 0){
+	            stake -=bet;
+	            System.out.println("gambler is loose by "+bet+" & total Stake is "+ stake );
+	        }
+	    }
 }
