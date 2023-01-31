b9d9dbf1e94e72c7bf46840ec0ba5ac394afb215
sumi <sumipavib1302@gmail.com>
2023-01-31 22:27:33 +0530

    Added:After 20 days of playing every day would like to know the total amount won or lost.

--- src/com/gamlingsimulation/GamblingSimulationProblem.java
+++ src/com/gamlingsimulation/GamblingSimulationProblem.java
@@ -4,29 +4,31 @@ import java.util.Random;
 	public class GamblingSimulationProblem {
 		
 		public static void main(String[] args) {
-	        System.out.println("Welcome in Gambling Simulation Problem");
-	        int stake = 100;
-	        int bet = 1;
-	        Random random = new Random();
-	        System.out.println("Initial stake " + stake);
-	        while (stake != 150 && stake != 50) {
-	            int num = random.nextInt(2);
-	            System.out.println("num is "+num);
-	            if (num == 1) {
-	                stake += bet;
+			 System.out.println("Welcome in Gambling Simulation Problem");
+		        int stake = 100;
+		        int bet = 1;
+		        int count = 0;
+		        Random random = new Random();
+		        System.out.println("Initial stake " + stake);
 
-	                //System.out.println("gambler is win by " + bet + " & total Stake is " + stake);
-	            } else if (num == 0) {
-	                stake -= bet;
-	              //  System.out.println("gambler is loose by " + bet + " & total Stake is " + stake);
-	            }
+		        int day = 20;
+		        for ( int i = 1;i <= day; i++ ){
+		            int num = random.nextInt(2);
 
-	        }
-	        if (stake >= 150) {
-	            System.out.println("player win 50% of its initial stake " + stake);
-	        } else {
-	            System.out.println("player loose 50% of its initial stake " + stake);
-	        }
+		            if (num == 1) {
+		                stake += bet;
+		                count++;
+		                System.out.println("gambler is win by day => "+count +  " & total Stake is " + stake);
 
+		            } else if (num == 0) {
+		                stake -= bet;
+		                count++;
+		               System.out.println("gambler is loose by day => "+count +  " & total Stake is " + stake);
+
+		            }else {
+		                System.out.println("player stop gambling for day "  );
+		            }
+
+		        }
 	    }
 }
